7ea4acb88c4310324b46337248dae552fa6ec308
James Jung <therealjamesjung@gmail.com>
2022-11-02 23:34:17 +0900

    pleasssse

--- parse.py
+++ parse.py
@@ -1,5 +1,8 @@
 from datanashor.parser import ReplayParser
 
-parser = ReplayParser(current_game_version='12.21', train=True,
+
+while True:
+    parser = ReplayParser(current_game_version='12.21', train=True,
                       train_api_root='http://13.124.221.110/api/v1/train_game/upload_json', serial_port='COM?????<-이거꼭바꿔라')
-parser.parse()
+    parser.parse()
+    
